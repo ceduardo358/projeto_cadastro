efe8057cb8d52a7e3be818229ea68fe6b721d8fc
Carlos Eduardo <ceduardo358@gmail.com>
2021-12-15 22:45:45 -0300

    criando projeto cadastro em php

---
+++ login.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Página de Login</title>
+</head>
+<body>
+
+<div class="container">
+
+    <h2>Efetue seu login</h2>
+
+    <form action="">
+        <input class="input" type="text" name="usuario" placeholder="Login...">
+
+        <input class="input" type="password" name="senha" placeholder="Senha!">
+
+        <input class="input" type="button" value="Acessar">
+    </form>
+
+    <p>Caso não seja cadastrado, <a href="cadastro.php">Clique aqui.</a> </p>
+    
+
+
+</div>
+    
+</body>
+</html>
